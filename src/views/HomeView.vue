76781de077f412e3f940c86d94baa4f7fe566908
patipan-patisampita <patipan-patisampita@gmail.com>
2024-02-01 11:32:18 +0700

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,29 @@
 <template>
-  <div class="container">
-    <h1>Home</h1>
+  <div class="container my-5">
+    <div class="row">
+      <div class="col-md-6">
+
+        <div class="card shadow">
+          <div class="card-header p-0">
+            <img src="https://cdn.pixabay.com/photo/2017/01/14/15/11/relaxing-1979674_640.jpg" class="card-img-top"
+              alt="img">
+          </div>
+          <div class="card-body">
+            <h5 class="cart-title">Home Page</h5>
+            <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum quaerat perferendis
+              quas eum doloribus molestiae!
+            </p>
+            <a href="#" class="btn btn-primary btn-sm">อ่านต่อ...</a>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="col-md-6">
+
+      </div>
+
+    </div>
   </div>
 </template>
 
@@ -8,6 +31,4 @@
 
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
