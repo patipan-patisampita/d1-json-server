779a3da38d9dab1f2b33a60b4bac6a8671110276
patipan-patisampita <patipan-patisampita@gmail.com>
2024-02-01 11:38:28 +0700

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container my-5">
     <div class="row">
-      <div class="col-md-6">
+      <div class="col-md-3">
 
         <div class="card shadow">
           <div class="card-header p-0">
@@ -9,7 +9,7 @@
               alt="img">
           </div>
           <div class="card-body">
-            <h5 class="cart-title">Home Page</h5>
+            <h5 class="cart-title">หน้าแรก</h5>
             <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum quaerat perferendis
               quas eum doloribus molestiae!
             </p>
@@ -31,4 +31,14 @@
 
 </script>
 
-<style lang="scss" scoped></style>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Sarabun&display=swap');
+
+h5,p {
+  font-family: 'Sarabun', sans-serif;
+}
+
+a{
+  font-family: 'Sarabun', sans-serif;
+}
+</style>
