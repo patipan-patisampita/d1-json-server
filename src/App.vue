d4310b53bdad72704fde5fc5c10c53dff162a568
patipan-patisampita <patipan-patisampita@gmail.com>
2024-02-01 10:13:26 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg nabar-dark shadow stickey-top" style="background: #7A0FB6;">
     <div class="container">
-      <a class="navbar-brand" href="#">สาขาไอที</a>
+      <router-link :to="{name: 'home'}" class="navbar-brand">สาขาไอที</router-link>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -9,7 +9,7 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">หน้าแรก</a>
+            <router-link :to="{ name: 'home' }" class="nav-link active" aria-current="page" href="#">หน้าแรก</router-link >
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Link</a>
